2cd32cfbc85fa1b01fa6d9e49be72415edd4ccd4
Paul Richter <prichter828@gmail.com>
2015-11-17 22:53:26 -0500

    Adding ability to specify variable range of grams in a word. Also spliting consonant and vowel grams and alternating between the two when picking the next gram.

--- name_generator.py
+++ name_generator.py
@@ -1,4 +1,17 @@
-from gram_frequency import ALL_GRAMS, ALL_GRAM_WEIGHT, WORD_BEGIN, WORD_BEGIN_WEIGHT, WORD_END, WORD_END_WEIGHT
+from gram_frequency import (
+	ALL_GRAMS_LETTERS, 
+	ALL_GRAMS_VOWELS, 
+	ALL_GRAMS_LETTERS_WEIGHT, 
+	ALL_GRAMS_VOWELS_WEIGHT,
+	WORD_BEGIN_LETTER, 
+	WORD_BEGIN_VOWEL,
+	WORD_BEGIN_LETTER_WEIGHT,
+	WORD_BEGIN_VOWEL_WEIGHT, 
+	WORD_END_LETTER,
+	WORD_END_VOWEL, 
+	WORD_END_LETTER_WEIGHT,
+	WORD_END_VOWEL_WEIGHT
+)
 import random
 
 def generate_name(gram_count=4, name_count=2):
@@ -6,21 +19,47 @@ def generate_name(gram_count=4, name_count=2):
 	
 	for name_count_index in range(name_count):
 		name = ""
-		for gram_count_index in range(gram_count):
+		use_vowel = bool(random.getrandbits(1))
+		
+		if type(gram_count) is tuple:
+			target_gram_count = int(random.uniform(gram_count[0], gram_count[1] + 1))
+		else:
+			target_gram_count = gram_count
+		for gram_count_index in range(target_gram_count):
 			if gram_count_index == 0:
-				rand_val = random.uniform(0, WORD_BEGIN_WEIGHT)
-				gram = fetch_gram(rand_val, WORD_BEGIN)
-			elif gram_count_index == (gram_count - 1):
-				rand_val = random.uniform(0, WORD_END_WEIGHT)
-				gram = fetch_gram(rand_val, WORD_END)
+				if use_vowel:
+					start_gram_collection = WORD_BEGIN_VOWEL
+					start_gram_weight = WORD_BEGIN_VOWEL_WEIGHT
+				else:
+					start_gram_collection = WORD_BEGIN_LETTER
+					start_gram_weight = WORD_BEGIN_LETTER_WEIGHT
+					
+				rand_val = random.uniform(0, start_gram_weight)
+				gram = fetch_gram(rand_val, start_gram_collection)
+			elif gram_count_index == (target_gram_count - 1):
+				if use_vowel:
+					end_gram_collection = WORD_END_VOWEL
+					end_gram_weight = WORD_END_VOWEL_WEIGHT
+				else:
+					end_gram_collection = WORD_END_LETTER
+					end_gram_weight = WORD_BEGIN_LETTER_WEIGHT
+				rand_val = random.uniform(0, end_gram_weight)
+				gram = fetch_gram(rand_val, end_gram_collection)
 			else:
-				rand_val = random.uniform(0, ALL_GRAM_WEIGHT)
-				gram = fetch_gram(rand_val, ALL_GRAMS)
+				if use_vowel:
+					gram_collection = ALL_GRAMS_VOWELS
+					gram_weight = ALL_GRAMS_VOWELS_WEIGHT
+				else:
+					gram_collection = ALL_GRAMS_LETTERS
+					gram_weight = ALL_GRAMS_LETTERS_WEIGHT
+				rand_val = random.uniform(0, gram_weight)
+				gram = fetch_gram(rand_val, gram_collection)
 			
 			name += gram
+			use_vowel = not use_vowel
 		names.append(name)
 	
-	return " ".join(names)
+	return " ".join(names).title()
 
 def fetch_gram(rand_val, gram_list):
 	upto = 0
